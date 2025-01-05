1871552bac63e8de3e5b4b98f0c306c2ede0c6bd
Pawel Borkar <pawel@duck.com>
2025-01-06 03:43:11 +0530

    refactor: change upload method

--- api/app.ts
+++ api/app.ts
@@ -1,5 +1,5 @@
 import { env, loadEnvFile } from "node:process";
-import express, { Application } from "express";
+import express, { Application, Response } from "express";
 import helmet from "helmet";
 import cors from "cors";
 import hpp from "hpp";
@@ -10,10 +10,14 @@ const PORT = env.PORT || 8000;
 
 const app: Application = express();
 
+app.use(express.json());
 app.use(cors());
 app.use(hpp());
 app.use(helmet());
 
+app.get(`/api/${env.API_VERSION}/health`, (_, res) => {
+  res.send("This site is healthy");
+});
 app.use(`/api/${env.API_VERSION}/content`, router);
 
 app.listen(PORT, () => {
