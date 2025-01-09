54579d49313d38663ae884fbe5c572dd46a8868c
Pawel Borkar <pawel@duck.com>
2025-01-09 13:40:49 +0530

    chores: clean up

--- api/app.ts
+++ api/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, Response } from "express";
+import express, { Application } from "express";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import cors from "cors";
